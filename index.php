3ee6e755ec3db66744c07ed8633113bc4f0e2a36
= <=>
2020-10-27 19:33:37 +0700

    Homework

--- index.php
+++ index.php
@@ -1,4 +1,65 @@
-<?php $j=1; ?>
+<?php 
+	require("config.php");
+
+	$district = mysqli_query($conn, "Select * from nv4_vi_location_district");
+	$province = mysqli_query($conn, "Select * from nv4_vi_location_province");
+	$ward = mysqli_query($conn, "Select * from nv4_vi_location_ward");
+
+	if($_SERVER["REQUEST_METHOD"] == "POST"){
+	$error = array();
+		//Validate
+    if(empty($_POST['name'])){
+        $error['name'] = 'Bạn chưa nhập tên';
+    }else{
+    	$name = $_POST['name'];
+    }
+
+    if(empty($_POST['phone'])){
+        $error['phone'] = 'Bạn chưa nhập số điện thoại';
+    }else{
+    	$phone = $_POST['phone'];
+    }
+
+    if(empty($_POST['email'])){
+        $error['email'] = 'Bạn chưa nhập email';
+    }else{
+    	$email = $_POST['email'];
+    }
+
+    if(empty($_POST['province'])){
+        $error['province'] = 'Bạn chưa nhập Tỉnh';
+    }else{
+    	$province = $_POST['province'];
+    }
+
+    if(empty($_POST['district'])){
+        $error['district'] = 'Bạn chưa nhập Quận';
+    }else{
+    	$district = $_POST['district'];
+    }
+
+	if(empty($_POST['ward'])){
+        $error['ward'] = 'Bạn chưa nhập Xã';
+    }else{
+    	$ward = $_POST['ward'];
+    }
+
+}
+
+    $query = mysqli_query($conn, "INSERT INTO information (name, phone, email, province, district, ward) values 
+    	('$name', '$phone', '$email', '$province', '$district', '$ward')");
+		echo "<pre>";
+		print_r("INSERT INTO information (name, phone, email, province, district, ward) values ('$name', '$phone', '$email', '$province', '$district', '$ward')");	
+		echo "</pre>";
+
+		if($query){
+			header('location: form_tinh_thanh.php');
+      }else{
+         mysqli_error($conn);
+      }
+
+
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,50 +68,79 @@
 	<title></title>
 	<link rel="stylesheet" href="bootstrap-3.4.1-dist/css/bootstrap.min.css">
 	<link rel="stylesheet" href="bootstrap-3.4.1-dist/css/style.css">
-	<style>
-		.img{
-			width: 500px;
-			height: 300px;
-		}
-		.img img{
-			width: 50%;
-		}
-	</style>
+	
 </head>
 <body>
 	<div class="container">
-		<?php for ($i=1; $i < 11 ; $i++) { ?>
-			<div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
-				<a href="" class="thumbnail" >
-					<div class="img"><img src="img/anh<?php echo $i; ?>.jpg"  alt=""></div>
-					<h4>Hình Ảnh <?php echo $i; ?></h4>
-				</a>
-				<button type="submit" class="btn btn-info" data-toggle="modal" data-target="#exampleModalable-<?php echo $i;?>">Click me</button>
-			</div>
-
-			<div class="modal fade" id="exampleModalable-<?php echo $i;?>" role = "dialog">
-				<div class="modal-dialog" role = "document">
-					<div class="modal-content">
-						<div class="modal-header">
-							<h5 >Hình ảnh <?php echo $i; ?></h5>
-									        
-						</div>
-						<div class="modal-body">
-							<p>Infomation images.</p> 
-							<img src="img/anh<?php echo $i;?>.jpg" alt="">
-						</div>
-						<div class="modal-footer">
-							 <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-						</div>
+		<div class="row">
+			<form action="" method="POST" role="form">
+				<legend>Mời bạn điền thông tin</legend>
+			
+				<div class="form-group">
+					<label for="">Họ và tên</label>
+					<input type="text" class="form-control" name="name" placeholder="Mời bạn nhập họ tên">
+					<span class="alert-success">
+						<?php echo isset($error['name']) ? $error['name'] : ''; ?>
+					</span>
+				</div>
+				<div class="form-group">
+					<label for="">Điện thoại</label>
+					<input type="text" class="form-control" name="phone" placeholder="Mời bạn nhập điện thoại">
+					<span class="alert-success">
+					<?php echo isset($error['phone']) ? $error['phone'] : ''; ?>
+					</span>
+				</div>
+				<div class="form-group">
+					<label for="">Email</label>
+					<input type="email" class="form-control" name="email" placeholder="Mời bạn nhập email">
+					<span class="alert-success">
+					<?php echo isset($error['email']) ? $error['email'] : ''; ?>
+					</span>
+				</div>
+				<div class="form-group">
+					<label for="">Địa chỉ</label>
+					<div class="form-group">
+						<div class="col-md-4">
+						<select name="province" id="input" class="form-control" required="required">
+						<option value="">--Chọn tỉnh thành--</option>
+							<?php foreach ($province as $key => $TT) :?>
+							<option value="<?php echo $TT['id']?>"><?php echo $TT['title'] ?></option>
+						<?php endforeach ?>
+						</select>
+					</div>
+					<div class="col-md-4">
+						<select name="district" id="input" class="form-control" required="required">
+						<option value="">--Chọn Quận/Huyện--</option>
+						<?php foreach ($district as $key => $QH) :?>
+							<option value="<?php echo $QH['id']?>"><?php echo $QH['title'] ?></option>
+						<?php endforeach ?>
+						</select>
+					</div>
+					<div class="col-md-4">
+						<select name="ward" id="input" class="form-control" required="required">
+						<option value="">--Chọn Xã--</option>
+							<?php foreach ($ward as $key => $X) :?>
+								<option value="<?php echo $X['id']?>"><?php echo $X['title'] ?></option>
+							<?php endforeach ?>
+						</select>
+					</div>
 					</div>
 				</div>
-			</div>
-	<?php } ?>
+			
+				<div class=""><hr/></div>
+			
+				<div class="container">
+					<div class="row">
+						<div class="form-group">
+						<button type="submit" name="submit" class="btn btn-primary">Lưu</button>
+					</div>
+					</div>
+				</div>
+			</form>
+		</div>
 	</div>
-
-
-		<script src="bootstrap-3.4.1-dist/js/jquery.min.js" type="text/javascript"></script>
-		<script src="bootstrap-3.4.1-dist/js/bootstrap.min.js" type="text/javascript"></script>
+	<script src="bootstrap-3.4.1-dist/js/jquery.min.js" type="text/javascript"></script>
+	<script src="bootstrap-3.4.1-dist/js/bootstrap.min.js" type="text/javascript"></script>
 </body>
 </html>
 	
